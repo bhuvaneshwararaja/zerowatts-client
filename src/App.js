ad3e90527258f42e474045053dda706b72c3f186
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-07-12 13:41:38 +0530

    adding new Routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import LandingPage from './pages/landing';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route path="/" component={NavBar} />
+        <Route path="/home" component={Home} />
       </Switch>
     </Router>
    
