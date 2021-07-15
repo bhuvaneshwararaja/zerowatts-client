e0d0572c126e73745943b9baabe8fd635a0756e8
bhuvanesh <56244773+bhuvaneshwararaja@users.noreply.github.com>
2021-07-15 21:06:09 +0530

    updating landing UI

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -72,6 +72,7 @@ const LandingPage = () => {
                        </div>
                     </section>
                     
+                                                         
                 </form>
             </div>):""}
         </div>
