a460998ccdb43538d4677270b095a70dab23cfdc
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2021-07-19 23:05:57 +0530

    fixing UI

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,5 +1,5 @@
 
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import { FaGithubAlt} from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc"
 import React from 'react'
